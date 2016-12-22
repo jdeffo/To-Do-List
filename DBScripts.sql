ac17af4f56a842d33fdade679aa37eafe6421782
Jeremy DeFosset <jsd6695@gmail.com>
2016-12-22 01:18:47 -0500

    More initial setup, mainly mySQL and database things

---
+++ DBScripts.sql
@@ -0,0 +1,21 @@
+CREATE TABLE Tasks (
+  tid INT AUTO_INCREMENT,
+  title VARCHAR(100),
+  due_date DATE,
+  status VARCHAR(1)
+)
+engine=innodb;
+
+CREATE TABLE Completed (
+  tid INT,
+  title VARCHAR(100),
+  due_date DATE,
+  comp_date DATE,
+  status VARCHAR(1)
+)
+engine=innodb;
+
+INSERT INTO Tasks(title, due_date, status)
+VALUES ('Test 1', '2016-12-31', '0'),
+  ('Test 2', '2016-12-15', '0'),
+  ('Test 3', '2016-12-31', '1');
