29225b0f98dabe4badbe0995e8ae77a54fc57cbd
Jeremy DeFosset <jsd6695@gmail.com>
2017-05-09 02:52:58 -0400

    read and write are now functions

--- V1.py
+++ V1.py
@@ -1,25 +1,22 @@
-#File paths: printf(r'C:\User\....'')
+#global variables
+todoList = []
 
-#print the direction
-print("File path: " + r'C:\Users\Deffo\Source\Repos\To-Do-List\list.txt')
+#read function
+def listRead():
+    print("File path: " + r'C:\Users\Deffo\Source\Repos\To-Do-List\list.txt')
+    global todoList
+    with open("list.txt") as listf:
+        todoList = [line.rstrip('\n') for line in listf]
+    print(todoList)
+    listf.close()
 
-#open the File
-with open("list.txt") as listf:
-    todoList = [line.rstrip('\n') for line in listf]
-print (todoList)
+def listWrite(item):
+    listWr = open('list.txt', 'w')
+    for line in todoList:
+        listWr.write(line + "\n")
+    listWr.write(item)
+    listWr.close()
 
-#close the file
-listf.close()
-#write to the File
-listWr = open('list.txt', 'w')
-for line in todoList:
-    listWr.write(line + "\n")
-listWr.write("Finish V1 | Friday")
-
-listWr.close()
-
-with open('list.txt') as listCh:
-    newContents = [line.rstrip('\n') for line in listCh]
-print(newContents)
-
-listCh.close()
+listRead()
+listWrite("Aye | Wednesday")
+listRead()
